ab131461cc296e5804587a863e3e3cc6c52f48dd
Hugo Souza <deSouza@protonmail.com>
2022-10-06 20:39:38 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Support\Facades\Route;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use \App\Models\Category;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+
+    return view('posts',[
+        'posts' => Post::latest('published_at')->with('category','user')->get()
+    ]);
+    
+});
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post',[
+        'post' => $post
+    ]);
+});
+Route::get('categories/{category:slug}',function (Category $category){
+    return view('posts',[
+        'posts' => $category->posts
+    ]);
+});
+
+Route::get('users/{user:username}',function (\App\Models\User $user){
+    return view('posts',[
+        'posts' => $user->posts
+    ]);
+});
