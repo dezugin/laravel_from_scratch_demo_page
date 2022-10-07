197ddc55fec2ef8cedb5cf821c806a6cf8db0c45
Hugo Souza <deSouza@protonmail.com>
2022-10-07 14:59:27 -0300

    selection display

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -19,7 +19,7 @@
                         @click="show = !show"
                         class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
                         >
-                        {{isset($currentCategory) ?$currentCategory->name : 'Categories'}}
+                        {{isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories'}}
                      
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                          height="22" viewBox="0 0 22 22">
@@ -32,9 +32,15 @@
                     </svg>
                     </button>
                         <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl w-full z-50" style="display:none">
+                            <a href="/" 
+                            class = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white" >
+                            All</a>
                             @foreach($categories as $category)    
                                 <a href="/categories/{{$category->slug}}" 
-                                class = "block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white" >
+                                class = 
+                                "block text-left px-3 text-sm leading-6
+                                 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                                 {{isset($currentCategory)&&$currentCategory->id == $category->id ? 'bg-blue-500 text-white' : ''}} ">
                                 {{ucwords($category->name)}}</a>
                             @endforeach
                         </div>
