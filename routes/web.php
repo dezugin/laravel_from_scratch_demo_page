d61343ec0611ba512d678fa32506d69818d20acf
Hugo Souza <deSouza@protonmail.com>
2022-10-07 19:43:08 -0300

    Post Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 use \App\Models\Category;
@@ -15,22 +17,7 @@ use \App\Models\Category;
 |
 */
 
-Route::get('/', function () {
-
-    $posts = Post::latest();
-
-    if(request('search')){
-        $posts 
-            ->where('title','like','%' . request('search') . '%')
-            ->orWhere('body','like','%' . request('search') . '%');
-    }
-
-    return view('posts',[
-        'posts' => $posts->get(),
-        'categories' => Category::all()
-    ]);
-    
-})->name('home');
+Route::get('/', [PostController::class, 'index'] )->name('home');
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post',[
         'post' => $post
