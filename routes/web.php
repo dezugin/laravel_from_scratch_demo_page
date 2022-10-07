700710c85a805c7ec8973fe468d7f1e86c77dbd2
Hugo Souza <deSouza@protonmail.com>
2022-10-07 17:29:11 -0300

    home highlight

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
         'categories' => Category::all()
     ]);
     
-});
+})->name('home');
 Route::get('posts/{post:slug}', function (Post $post) {
     return view('post',[
         'post' => $post
@@ -35,7 +35,7 @@ Route::get('categories/{category:slug}',function (Category $category){
         'currentCategory' =>$category,
         'categories' => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('users/{user:username}',function (\App\Models\User $user){
     return view('posts',[
