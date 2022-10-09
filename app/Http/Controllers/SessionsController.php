019ad0528b1452e178c547092e8e0cd7e80d7fb4
Hugo Souza <deSouza@protonmail.com>
2022-10-09 20:32:07 -0300

    login page built

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -5,6 +5,29 @@ namespace App\Http\Controllers;
 
 class SessionsController extends Controller
 {
+
+    public function create(){
+        return view('sessions.create');
+    }
+    public function store(){
+        $attributes = request()->validate([
+            'email'=>'required|email',
+            'password'=>'required'
+        ]);
+        if(auth()->attempt($attributes)){
+            session()->regenerate();
+            return redirect('/')->with('success','Welcome Bach');
+        }
+
+    //     throw ValidationException::withMessages([
+    //         'email' => 'Your provided credentials could not be Verified'
+    //     ]);
+        else{   return back()
+              ->withInput()
+              ->withErrors(['email'=>'Your provided credentials could not be verified']);
+            }
+
+    }
     public function destroy(){
         auth()->logout();
 
